2287b8df753c98c8d5091f352482ce5e6d8783fc
Alexander Stepanov <alerstov@gmail.com>
2012-08-05 21:10:09 +0400

    Initial Commit

---
+++ FadeAnimation/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  FadeAnimation
+//
+//  Created by Lion User on 05.08.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ FadeAnimation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FadeAnimation
+//
+//  Created by Lion User on 05.08.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
